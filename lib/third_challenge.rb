5e6cd0524c10eabcc77abb36c1215c7d71000ac9
Nicolas Marini <nmarini44@gmail.com>
2018-05-31 00:47:05 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -17,8 +17,19 @@ def third_challenge
         age: "15",
         status: "alive"
         },
-      :hero_friends => []
-   }, 
+      :hero_friends => [
+        {
+          name: "Benvolio",
+          age: "17",
+          attitude: "worried"
+        },
+        {
+        name: "Mercutio",
+        age: "18",
+        attitude: "hot-headed"  
+        }
+      ]
+   },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
@@ -36,9 +47,12 @@ def third_challenge
         age: "15",
         status: "alive"
         },
-      :heroine_friends => []
+      :heroine_friends => [
+        {name: "Steven", age: "30", attitude: "confused"},
+          {name: "Nurse", age: "44", attitude: "worried"}
+      ]
    }
   }
 
-  
+
 end
